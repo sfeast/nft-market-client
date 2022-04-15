7b1a0c4b3ab8a3431508ee804ce8e09eb1c7bf70
Eugene B <eugene.bugor@gmail.com>
2022-04-15 02:29:48 +0200

    tooltip

--- src/components/NftMarketActions/BuyNow/index.js
+++ src/components/NftMarketActions/BuyNow/index.js
@@ -1,29 +1,35 @@
 import { useDispatch, useSelector } from 'react-redux';
 
 import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
 
 import { marketActions } from 'store/actions';
 import { walletSelectors } from 'store/selectors';
+import { getPrice } from 'utils/normalizers/nftItem';
 
 const Index = ({ tokenId, price, ...props }) => {
     const dispatch = useDispatch();
     const balance = useSelector(walletSelectors.selectBalance);
+
+    const disabled = balance < price;
 
     const onClick = () => {
         dispatch(marketActions.buyListing(tokenId, price));
     };
 
     return (
-        <Button
-            onClick={onClick}
-            variant="contained"
-            color="secondary"
-            sx={{ color: 'text.light', fontWeight: 'bold' }}
-            disabled={Number(balance) < Number(price)}
-            {...props}
-        >
-            Buy now
-        </Button>
+        <Tooltip title={disabled ? `Your balance is ${getPrice(balance, 2)}` : ''}>
+            <Button
+                onClick={onClick}
+                variant="contained"
+                color="secondary"
+                sx={{ color: 'text.light', fontWeight: 'bold' }}
+                disabled={disabled}
+                {...props}
+            >
+                Buy now
+            </Button>
+        </Tooltip>
     );
 };
 
